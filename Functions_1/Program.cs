71dd64e04bbec227eed9adf18222777e95dd7c76
andre <andrej.kozhan00@mail.ru>
2021-06-23 01:01:17 +0400

    Change names and spaces

--- Functions_1/Program.cs
+++ Functions_1/Program.cs
@@ -10,67 +10,66 @@ namespace Functions_1
 {
     class Program
     {
-       static public void AddDossier(ref string[] FIO, ref string[] position)
+       static public void AddDossier(ref string[] fullNamesArray, ref string[] positionEmployeesArray)
         {
             Console.WriteLine("Введите фио сотрудника, которого хотите добавить");
-            string fio = Console.ReadLine();
+            string inputFullNamesArray = Console.ReadLine();
             Console.WriteLine("Введите должность сотрудника, которого хотите добавить");
-            string pos = Console.ReadLine();
-
-            //изменение размера массива через ссылку на другой массив
-            string[] FIO_new = new string[FIO.Length+1] ;
-            for (int i = 0; i < FIO.Length; i++)
-                FIO_new[i] = FIO[i];          
-            string[] position_new = new string[position.Length + 1];
-            for (int i = 0; i < position.Length; i++)
-                position_new[i] = position[i];
-            FIO = FIO_new;
-            FIO[FIO.Length-1] = fio;
-            position = position_new;
-            position[position.Length-1] = pos;
+            string inputPositionEmployeesArray = Console.ReadLine();
+            string[] newFullNamesArray = new string[fullNamesArray.Length+1] ;
+           
+            for (int i = 0; i < fullNamesArray.Length; i++)
+                newFullNamesArray[i] = fullNamesArray[i];          
+            string[] newPositionEmployeesArray = new string[positionEmployeesArray.Length + 1];
+            
+            for (int i = 0; i < positionEmployeesArray.Length; i++)
+                newPositionEmployeesArray[i] = positionEmployeesArray[i];
+                       fullNamesArray = newFullNamesArray;
+            fullNamesArray[fullNamesArray.Length-1] = inputFullNamesArray;
+            positionEmployeesArray = newPositionEmployeesArray;
+            positionEmployeesArray[positionEmployeesArray.Length-1] = inputPositionEmployeesArray;
 
         }
 
-        static public void CheckDossier( string[] FIO,  string[] position)
+        static public void CheckDossier( string[] fullNamesArray,  string[] positionEmployeesArray)
         {
-            for (int i = 0; i < FIO.Length; i++)
-                Console.WriteLine($"Досье номер {i}\t ФИО: {FIO[i]}, должность: {position[i]}");         
+            for (int i = 0; i < fullNamesArray.Length; i++)
+                Console.WriteLine($"Досье номер {i}\t ФИО: {fullNamesArray[i]}, должность: {positionEmployeesArray[i]}");         
         }
 
-        static public void DeleteDossier(ref string[] FIO, ref string[] position)
-        {
-
-            //изменение размера массива через ссылку на другой массив
-            string[] FIO_new = new string[FIO.Length - 1];
-            for (int i = 0; i < FIO_new.Length; i++)
-                FIO_new[i] = FIO[i];
-            FIO = FIO_new;
-            string[] position_new = new string[position.Length - 1];
+        static public void DeleteDossier(ref string[] fullNamesArray, ref string[] positionEmployeesArray)
+        {         
+            string[] newFullNamesArray = new string[fullNamesArray.Length - 1];
+            for (int i = 0; i < newFullNamesArray.Length; i++)
+                newFullNamesArray[i] = fullNamesArray[i];
+            fullNamesArray = newFullNamesArray;
+            string[] position_new = new string[positionEmployeesArray.Length - 1];
+            
             for (int i = 0; i < position_new.Length; i++)
-                position_new[i] = position[i];
-            position = position_new;
+                position_new[i] = positionEmployeesArray[i];
+            positionEmployeesArray = position_new;
         }
-        static public void FindDossier(ref string[] FIO, ref string[] position)
+
+        static public void FindDossier(ref string[] fullNamesArray, ref string[] positionEmployeesArray)
         {
             Console.WriteLine("Введите фамилию человека, которого хотите найти ");
             string surname = Console.ReadLine();
-            Regex regex = new Regex($@"\s*{surname}\s*");//регулярное выражение
+            Regex regex = new Regex($@"\s*{surname}\s*");
 
-            for (int i = 0; i < FIO.Length; i++)
+            for (int i = 0; i < fullNamesArray.Length; i++)
             {              
-                if (regex.IsMatch(surname)) //соответствие регулярному выражению              
-                    Console.WriteLine($"ФИО: {FIO[i]}, должность: {position[i]}");                         
+                if (regex.IsMatch(surname))              
+                    Console.WriteLine($"ФИО: {fullNamesArray[i]}, должность: {positionEmployeesArray[i]}");                         
             }
          
         }
+
         static void Main(string[] args)
         {
-            string[] FIO = new string[] { };
-            string[] position = new string[] { };
-    
-
-           
+            string[] fullNamesArray = new string[] { };
+            string[] positionEmployeesArray = new string[] { }; 
             int caseSwitch = -1; 
+
             while (caseSwitch != 0)
             {
                 Console.WriteLine("Выберите номер действия:\n" +
@@ -85,16 +84,16 @@ namespace Functions_1
                 switch (caseSwitch)
                 {
                     case 1:
-                        AddDossier(ref FIO, ref position);
+                        AddDossier(ref fullNamesArray, ref positionEmployeesArray);
                         break;
                     case 2:
-                        CheckDossier(FIO, position);
+                        CheckDossier(fullNamesArray, positionEmployeesArray);
                         break;
                     case 3:
-                        DeleteDossier(ref FIO,ref position);
+                        DeleteDossier(ref fullNamesArray, ref positionEmployeesArray);
                         break;
                     case 4:
-                        FindDossier(ref FIO, ref position);
+                        FindDossier(ref fullNamesArray, ref positionEmployeesArray);
                         break;
                     case 0:
                         return;
